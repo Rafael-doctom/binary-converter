1359c80a6007fae8c9e3fa4cab05db8cdfdd6602
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-29 16:33:28 -0300

    feat: function that changes data and returns a bit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,18 @@
 var numberSendValue = document.getElementById("send-value");
 var result = document.getElementById("result");
 
-numberSendValue.addEventListener("click", functionEvent);
+numberSendValue.addEventListener("click", ValueGenerator);
 
-function functionEvent() {
+function ValueGenerator(numberTransform) {
 
     var numberTransform = document.getElementById("numberTransform").value;
 
-    result.innerHTML = `${numberTransform}`;
-    console.log(`Esse é o valor em binário: ${numberTransform}`);
-}
+    const bit = (numberTransform >>> 0).toString(2);
+    console.log(bit);
+
+    const data = numberTransform !== '' ? (numberTransform >>> 0).toString(2) : `<p>Nenhum valor adicionado.</p>`;
+
+    result.innerHTML = `${data}`;
+
+};
+
